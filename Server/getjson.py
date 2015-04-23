dd6aec2e7725966c81513605e6c2c01e3d576941
Kevin Coffey <kevin.d.coffey@monsanto.com>
2015-04-23 11:50:45 -0500

    gets forecast json

--- Server/getjson.py
+++ Server/getjson.py
@@ -1,3 +1,17 @@
+from urllib import request
+import json
 
 # get the api key
-api_key = open('./api.key').read()
+def _get_apikey():
+    return open('./api.key').read()
+
+def forecast(location):
+    # build up url
+    url = 'http://api.wunderground.com/api/'
+    url += _get_apikey() + '/forecast/q/' + location + '.json'
+    # send http request
+    forecast_req = request.urlopen(url)
+    # read response
+    forecast_res = forecast_req.read()
+    # return parsed json
+    return json.loads(forecast_res.decode())
